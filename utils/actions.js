c3a46d3e222ff679a0054757687a757129d7287e
heliosalesjr <heliosalesjr@gmail.com>
2024-03-07 17:02:06 -0300

    openai bug fix

--- utils/actions.js
+++ utils/actions.js
@@ -6,17 +6,19 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAY_API_KEY,
 });
 
-export const generateChatResponse = async (chatMessage) => {
-    const response = await openai.chat.completions.create({
+export const generateChatResponse = async (chatMessages) => {
+    
+      const response = await openai.chat.completions.create({
         messages: [
           { role: 'system', content: 'you are a helpful assistant' },
-          { role: 'user', content: 'chatMessage' },
+          { role: 'user', content: chatMessages },
         ],
         model: 'gpt-3.5-turbo',
         temperature: 0,
-        
+        max_tokens: 100,
       });
       console.log(response.choices[0].message);
       console.log(response);
-    return 'oi oi oi';
-}
+      return 'wow';
+
+    }
